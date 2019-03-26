3c803b0fd54914eabc8e11b52bc7272221e7551b
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-03-26 01:29:32 +0100

    add testing module

--- src/test/java/org/lightjason/agentspeak/action/graph/TestCActionGraph.java
+++ src/test/java/org/lightjason/agentspeak/action/graph/TestCActionGraph.java
@@ -33,10 +33,10 @@ import edu.uci.ics.jung.graph.SparseMultigraph;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import org.junit.Assert;
 import org.junit.Test;
-import org.lightjason.agentspeak.IBaseTest;
 import org.lightjason.agentspeak.language.CRawTerm;
 import org.lightjason.agentspeak.language.ITerm;
 import org.lightjason.agentspeak.language.execution.IContext;
+import org.lightjason.agentspeak.testing.IBaseTest;
 
 import java.util.ArrayList;
 import java.util.Collections;
